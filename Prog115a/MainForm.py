c76c0b38bc65c61099801527a427012e07c497dc
trumpyb <91992867+trumpyb@users.noreply.github.com>
2024-01-30 14:16:19 -0600

    01/30/2024

---
+++ Prog115a/MainForm.py
@@ -0,0 +1,84 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._listBox1 = System.Windows.Forms.ListBox()
+		self._Calc = System.Windows.Forms.Button()
+		self._clr = System.Windows.Forms.Button()
+		self._ext = System.Windows.Forms.Button()
+		self.SuspendLayout()
+		# 
+		# listBox1
+		# 
+		self._listBox1.FormattingEnabled = True
+		self._listBox1.Location = System.Drawing.Point(44, 27)
+		self._listBox1.Name = "listBox1"
+		self._listBox1.Size = System.Drawing.Size(120, 342)
+		self._listBox1.TabIndex = 0
+		# 
+		# Calc
+		# 
+		self._Calc.Location = System.Drawing.Point(170, 27)
+		self._Calc.Name = "Calc"
+		self._Calc.Size = System.Drawing.Size(75, 23)
+		self._Calc.TabIndex = 1
+		self._Calc.Text = "Calculate"
+		self._Calc.UseVisualStyleBackColor = True
+		self._Calc.Click += self.CalcClick
+		# 
+		# clr
+		# 
+		self._clr.Location = System.Drawing.Point(170, 180)
+		self._clr.Name = "clr"
+		self._clr.Size = System.Drawing.Size(75, 23)
+		self._clr.TabIndex = 2
+		self._clr.Text = "Clear"
+		self._clr.UseVisualStyleBackColor = True
+		self._clr.Click += self.ClrClick
+		# 
+		# ext
+		# 
+		self._ext.Location = System.Drawing.Point(170, 346)
+		self._ext.Name = "ext"
+		self._ext.Size = System.Drawing.Size(75, 23)
+		self._ext.TabIndex = 3
+		self._ext.Text = "Exit"
+		self._ext.UseVisualStyleBackColor = True
+		self._ext.Click += self.ExtClick
+		# 
+		# MainForm
+		# 
+		self.ClientSize = System.Drawing.Size(888, 442)
+		self.Controls.Add(self._ext)
+		self.Controls.Add(self._clr)
+		self.Controls.Add(self._Calc)
+		self.Controls.Add(self._listBox1)
+		self.Name = "MainForm"
+		self.Text = "Prog115a"
+		self.ResumeLayout(False)
+
+
+	def CalcClick(self, sender, e):
+		self._listBox1.Items.Clear()
+		
+		lcv = 2
+		
+		while lcv <= 36:
+			self._listBox1.Items.Add(str(lcv))
+			lcv += 2
+			
+			
+
+	def ClrClick(self, sender, e):
+		self._listBox1.Items.Clear()
+		
+
+	def ExtClick(self, sender, e):
+		Application.Exit()
---
+++ prog166e/MainForm.py
@@ -0,0 +1,81 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._listBox1 = System.Windows.Forms.ListBox()
+		self._calc = System.Windows.Forms.Button()
+		self._clr = System.Windows.Forms.Button()
+		self._ext = System.Windows.Forms.Button()
+		self.SuspendLayout()
+		# 
+		# calc
+		# 
+		self._calc.Location = System.Drawing.Point(170, 12)
+		self._calc.Name = "calc"
+		self._calc.Size = System.Drawing.Size(75, 23)
+		self._calc.TabIndex = 1
+		self._calc.Text = "Calculate"
+		self._calc.UseVisualStyleBackColor = True
+		self._calc.Click += self.CalcClick
+		# 
+		# clr
+		# 
+		self._clr.Location = System.Drawing.Point(170, 186)
+		self._clr.Name = "clr"
+		self._clr.Size = System.Drawing.Size(75, 23)
+		self._clr.TabIndex = 2
+		self._clr.Text = "Clear"
+		self._clr.UseVisualStyleBackColor = True
+		self._clr.Click += self.ClrClick
+		# 
+		# ext
+		# 
+		self._ext.Location = System.Drawing.Point(170, 380)
+		self._ext.Name = "ext"
+		self._ext.Size = System.Drawing.Size(75, 23)
+		self._ext.TabIndex = 3
+		self._ext.Text = "Exit"
+		self._ext.UseVisualStyleBackColor = True
+		self._ext.Click += self.ExtClick
+		# 
+		# MainForm
+		# 
+		self.ClientSize = System.Drawing.Size(892, 415)
+		self.Controls.Add(self._ext)
+		self.Controls.Add(self._clr)
+		self.Controls.Add(self._calc)
+		self.Controls.Add(self._listBox1)
+		self.Name = "MainForm"
+		self.Text = "prog166e"
+		self.Load += self.MainFormLoad
+		self.ResumeLayout(False)
+
+
+	def CalcClick(self, sender, e):
+		self._listBox.Items.Clear()
+		num = 1
+		den = 2
+		
+		while den<=15:
+			while num < den:
+				frac = float(num)/den
+				fstr = str(num)+"/"+str(den)
+				
+				self._listBox1.Items.Add(fstr + " = "+str(round(frac,5)))
+				
+				num += 1
+			num = 1
+			dem += 1
+
+	def ClrClick(self, sender, e):
+		self._listBox.Items.Clear()
+
+	def ExtClick(self, sender, e):
+		Application.Exit()
