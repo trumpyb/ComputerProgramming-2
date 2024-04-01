37208b118d255dc797620fd9510170b5c8f0771e
trumpyb <91992867+trumpyb@users.noreply.github.com>
2024-04-01 20:04:29 +0000

    Update

--- Cl702q.py
+++ Cl702q.py
@@ -7,20 +7,19 @@ class vehicle:
 class Car(vehicle):
   def __init__(self, worth, name, numTires):
     super().__init__(name, numTires)
-    self.milage = worth
+    self.worth = worth
 
 class Truck(vehicle):
   def __init__(self, name, numTires, milage):
     super().__init__(name, numTires)
     self.milage = milage
+    self.worth = 50000 - (.25*self.milage) 
 
-  def worth(self):
-    return 50000 - (.25*self.milage)
+  def __str__(self):
+    return f"{self.name}, {self.worth}"
     
 class Bus(vehicle):
   def __init__(self, name, numTires, hometown):
     super().__init__(name, numTires)
     self.hometown = hometown
-
-  def worth(self):
-    return 50000
+    self.worth = 50000
--- prog702q.py
+++ prog702q.py
@@ -1,21 +1,23 @@
 from Cl702q import *
+
+
 def main():
   busses = []
   cars = []
   trucks = []
   num = 0
   try:
-    with open("Dat/Langdat/dataFile.dat", "r") as f:
-      while num!=99:
+    with open("Dat/Langdat/prog702q.txt", "r") as f:
+      while num != 99:
         num = int(f.readline())
         d1 = f.readline()
         d2 = f.readline()
         d3 = f.readline()
-        if num==1:
-          cars.append(Car(int(d1), d2, int(d3)))
-        if num==2:
+        if num == 1:
+          cars.append(Car(int(d3), d1, int(d2)))
+        if num == 2:
           trucks.append(Truck(d1, int(d2), int(d3)))
-        if num==3:
+        if num == 3:
           busses.append(Bus(d1, int(d2), d3))
 
     totVehicles = len(cars) + len(trucks) + len(busses)
@@ -27,18 +29,18 @@ def main():
     totVehicleWorth = totCarWorth
     for x in trucks:
       totVehicleWorth += x.worth
-    for x in busses:
-      totVehicleWorth += x.worth
+    for y in busses:
+      totVehicleWorth += y.worth
 
     longestname = ""
     for x in busses:
       if len(x.hometown) > len(longestname):
         longestname = x.hometown
 
-    leastValue = 10**99
+    leastValue = Truck("CyberDuck", 4, -220)
     for x in trucks:
-      if x.worth < leastValue:
-        leastValue = x.worth
+      if x.worth < leastValue.worth:
+        leastValue = x
 
     carTires = 0
     for x in cars:
@@ -51,7 +53,7 @@ def main():
     busTires = 0
     for x in busses:
       busTires += x.numTires
-    
+
     print(f"Total Vehicles: {totVehicles}")
     print(f"Total Car Worth: {totCarWorth}")
     print(f"Total Vehicle Worth: {totVehicleWorth}")
@@ -60,11 +62,11 @@ def main():
     print(f"Total Car Tires: {carTires}")
     print(f"Total Bus Tires: {busTires}")
     print(f"Total Truck Tires: {truckTires}")
-  
+
   except Exception as e:
 
     print("Error", e)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
   main()
