0d4a5684c655ab8bd0e7abc552bb12e6c888a643
trumpyb <91992867+trumpyb@users.noreply.github.com>
2024-04-01 19:47:28 +0000

    Update

--- Cl702q.py
+++ Cl702q.py
@@ -4,7 +4,23 @@ class vehicle:
     self.numTires = numTires
     
 
-class car(vehicle):
-  def __init__(self, milage, name, numTires):
+class Car(vehicle):
+  def __init__(self, worth, name, numTires):
+    super().__init__(name, numTires)
+    self.milage = worth
+
+class Truck(vehicle):
+  def __init__(self, name, numTires, milage):
     super().__init__(name, numTires)
     self.milage = milage
+
+  def worth(self):
+    return 50000 - (.25*self.milage)
+    
+class Bus(vehicle):
+  def __init__(self, name, numTires, hometown):
+    super().__init__(name, numTires)
+    self.hometown = hometown
+
+  def worth(self):
+    return 50000
--- prog702q.py
+++ prog702q.py
@@ -1,21 +1,9 @@
-"""
-You are to calculate the total number of vehicles.
-
-You are to calculate the total amount that the cars are worth.  
-
-You are to calculate the total amount that the vehicles are worth.  
-
-You are to report the longest home destination name for all of the busses.  
-
-You are to report which truck has the least value.  
-
-You are to report the total number of tires in each of the three classes of vehicles.
-"""
-
+from Cl702q import *
 def main():
-  bus = []
-  car = []
-  truck = []
+  busses = []
+  cars = []
+  trucks = []
+  num = 0
   try:
     with open("Dat/Langdat/dataFile.dat", "r") as f:
       while num!=99:
@@ -24,13 +12,54 @@ def main():
         d2 = f.readline()
         d3 = f.readline()
         if num==1:
-          
+          cars.append(Car(int(d1), d2, int(d3)))
         if num==2:
-          
+          trucks.append(Truck(d1, int(d2), int(d3)))
         if num==3:
-          
+          busses.append(Bus(d1, int(d2), d3))
 
+    totVehicles = len(cars) + len(trucks) + len(busses)
 
+    totCarWorth = 0
+    for x in cars:
+      totCarWorth += x.worth
+
+    totVehicleWorth = totCarWorth
+    for x in trucks:
+      totVehicleWorth += x.worth
+    for x in busses:
+      totVehicleWorth += x.worth
+
+    longestname = ""
+    for x in busses:
+      if len(x.hometown) > len(longestname):
+        longestname = x.hometown
+
+    leastValue = 10**99
+    for x in trucks:
+      if x.worth < leastValue:
+        leastValue = x.worth
+
+    carTires = 0
+    for x in cars:
+      carTires += x.numTires
+
+    truckTires = 0
+    for x in trucks:
+      truckTires += x.numTires
+
+    busTires = 0
+    for x in busses:
+      busTires += x.numTires
+    
+    print(f"Total Vehicles: {totVehicles}")
+    print(f"Total Car Worth: {totCarWorth}")
+    print(f"Total Vehicle Worth: {totVehicleWorth}")
+    print(f"Longest Home Name: {longestname}")
+    print(f"Truck With Least Value: {leastValue}")
+    print(f"Total Car Tires: {carTires}")
+    print(f"Total Bus Tires: {busTires}")
+    print(f"Total Truck Tires: {truckTires}")
   
   except Exception as e:
 
